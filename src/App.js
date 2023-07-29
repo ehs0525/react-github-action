a300433ddaf0c83582ee5ce804646cccb228bec8
엄희성 <ehs0525@naver.com>
2023-07-30 04:37:09 +0900

    Counter & Test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useCallback, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  const addCount = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  const subtractCount = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
+  const disableButtons = useCallback(() => {
+    setDisabled((prev) => !prev);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3 data-testid="counter">{count}</h3>
+
+        <button
+          data-testid="minus-button"
+          onClick={subtractCount}
+          disabled={disabled}
         >
-          Learn React
-        </a>
+          -
+        </button>
+
+        <button
+          data-testid="plus-button"
+          onClick={addCount}
+          disabled={disabled}
+        >
+          +
+        </button>
+
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid="on/off-button"
+            onClick={disableButtons}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
